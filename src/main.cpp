a293aa2cbbd5cc48e4f6b8f0a316f8e33f9efc97
Marc Villacorta <marc.villacorta@gmail.com>
2010-12-13 18:24:21 +0100

    log4cxx

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 #define PORT 6666
 #define LISTENQ 512
 #define MAXLINE 512
-#define MyDBG printf("Error: %s:%d", __FILE__, __LINE__);
+#define MyDBG LOG4CXX_WARN(logger, "Return on error.");
 
 //-----------------------------------------------------------------------------
 // Typedefs:
@@ -26,9 +26,18 @@
 typedef void Sigfunc (int);
 
 //-----------------------------------------------------------------------------
+// log4cxx:
+//-----------------------------------------------------------------------------
+
+using namespace log4cxx;
+using namespace log4cxx::helpers;
+static LoggerPtr logger(Logger::getLogger("strBridge"));
+
+//-----------------------------------------------------------------------------
 // Headers:
 //-----------------------------------------------------------------------------
 
+int init_daemon(void);
 int dispatch_start (int clientfd);
 int dispatch_stop (int clientfd);
 Sigfunc *signal (int signo, Sigfunc *func);
@@ -43,7 +52,7 @@ int addDestination(int src_id, int dest_id, char* ip, int port);
 // Globals:
 //-----------------------------------------------------------------------------
 
-Manager * sm;
+Manager * sm = NULL;
 
 //-----------------------------------------------------------------------------
 // Entry point:
@@ -52,21 +61,25 @@ Manager * sm;
 int main(void)
 
 {
-	//-----------------
 	// Init variables:
-	//-----------------
-
-	sm = new Manager();
 	int data, listenfd, clientfd;
 	struct sockaddr_in servaddr, cliaddr;
 	socklen_t len = sizeof(cliaddr);
 	pid_t pid;
 	char frameid[1];
 
-	//--------------------------
-	// Socket: bind and listen:
-	//--------------------------
+	// Configure the logger:
+	log4cxx::PropertyConfigurator::configure(log4cxx::File("conf/logger.properties"));
 
+	// Detach process:
+	if(init_daemon() < 0) {goto halt0;}
+	LOG4CXX_WARN(logger, "Detached from parent.");
+
+	// Instantiate the manager interface:
+	if((sm = new Manager()) == NULL) {goto halt0;}
+	LOG4CXX_WARN(logger, "New manager interface.");
+
+	// Socket: bind and listen:
 	if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {MyDBG; goto halt0;}
 	if(setsockopt(listenfd, SOL_SOCKET, SO_KEEPALIVE, &data, sizeof(data)) < 0) {MyDBG; goto halt1;}
 
@@ -77,43 +90,26 @@ int main(void)
 
 	if(bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {MyDBG; goto halt1;}
 	if(listen(listenfd, LISTENQ) < 0) {MyDBG; goto halt1;}
+	LOG4CXX_WARN(logger, L"Listening on port: 6666");
 
-	//-------------------
 	// Signal callbacks:
-	//-------------------
-
 	if((signal(SIGCHLD, sig_chld)) == SIG_ERR) {MyDBG; goto halt1;}
 
-	//------------
 	// Main loop:
-	//------------
-
 	while(1)
 
 	{
-		//-----------------------------
 		// Accept a client connection:
-		//-----------------------------
-
 		if((clientfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len)) < 0) continue;
 
-		//--------------------
 		// Concurrent server:
-		//--------------------
-
 		if((pid = fork()) == 0)
 
 		{
-			//---------------------------------------
 			// Do not duplicate 'listenfd' in child:
-			//---------------------------------------
-
 			close(listenfd);
 
-			//-------------------------------
 			// Get a valid frame ID or drop:
-			//-------------------------------
-
 			readn(clientfd, frameid, 1);
 
 			switch(frameid[0])
@@ -124,27 +120,45 @@ int main(void)
 				default: printf("Frame drop: [%c]", frameid[0]);
 			}
 
-			//------------
 			// Child end:
-			//------------
-
 			close(clientfd);
 			exit(0);
 		}
 
-		//---------------------
 		// End of parent loop:
-		//---------------------
-
 		close(clientfd);
 	}
 
-	//------------------
 	// Return on error:
-	//------------------
-
 	halt1: close(listenfd);
 	halt0: return 1;
+}
+
+//-----------------------------------------------------------------------------
+// init_daemon:
+//-----------------------------------------------------------------------------
+
+int init_daemon(void)
+
+{
+	pid_t pid;
+
+	// Forking the parent process:
+	if((pid = fork()) < 0) return -1;
+	if(pid > 0) exit(0);
+
+	// Setting up the environment:
+	umask(0);
+	if(setsid() < 0) return -1;
+	if((chdir("/")) < 0) return -1;
+
+	// Closing Standard File Descriptors:
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	// Return on success.
+	return 0;
 }
 
 //-----------------------------------------------------------------------------
@@ -154,61 +168,38 @@ int main(void)
 int dispatch_start (int clientfd)
 
 {
-	//-----------------
 	// Init variables:
-	//-----------------
-
 	char buff1[MAXLINE], buff2[MAXLINE], *c=buff1;
 	int i=0, j=0, k=0, p=(int)getpid();
 
-	//----------------------
 	// Retrieve the buffer:
-	//----------------------
-
 	if((i = readn(clientfd, buff1, 2)) != 2) {MyDBG; goto halt0;}
 	buff1[i]='\0'; if((j = atoi(buff1)) == 0) {MyDBG; goto halt0;}
 	if((i = readn(clientfd, buff1, j)) != j) {MyDBG; goto halt0;}
 	buff1[i]='\0'; j=0;
 
-	//-------------------
 	// Split the buffer:
-	//-------------------
-
 	while(sscanf(c, "%[^:]:%d", buff2, &i) == 2)
 
 	{
-		printf("PID: %d [%s]:[%d]\n", p, buff2, i);
-
-		//------------------------------
 		// Add source and destinations:
-		//------------------------------
-
 		if(j==0){k += addSource(1, p, buff2, i); j++;}
 		else{k += addDestination(p, j, buff2, i); j++;}
 
-		//---------------------------
 		// Evaluate break condition:
-		//---------------------------
-
 		c += (strlen(buff2)+digits(i)+2)*sizeof(char);
 		if(*(c-sizeof(char)) == '\0') {break;}
 	}
 
 	if (k != 0) {MyDBG; goto halt0;}
 
-	//---------------------------------
 	// Send PID close socket and loop:
-	//---------------------------------
-
 	if(sprintf(buff1, "%d", p) < 0) {MyDBG; goto halt0;};
 	writen(clientfd, buff1, strlen(buff1));
 	close(clientfd);
 	while(1);
 
-	//------------------
 	// Return on error:
-	//------------------
-
 	halt0: return -1;
 }
 
@@ -219,39 +210,24 @@ int dispatch_start (int clientfd)
 int dispatch_stop (int clientfd)
 
 {
-	//-----------------
 	// Init variables:
-	//-----------------
-
 	char buff[MAXLINE];
 	int i, j;
 
-	//----------------------
 	// Retrieve the buffer:
-	//----------------------
-
 	if((i = readn(clientfd, buff, 1)) != 1) {MyDBG; goto halt0;}
 	buff[i]='\0'; if((j = atoi(buff)) == 0) {MyDBG; goto halt0;}
 	if((i = readn(clientfd, buff, j)) != j) {MyDBG; goto halt0;}
 	buff[i]='\0'; j=0;
 
-	//--------------------------
 	// Get PID/SID and kill it:
-	//--------------------------
-
 	if(sscanf(buff, "%d", &i) != 1) {MyDBG; goto halt0;}
 	kill(i, SIGINT);
 
-	//--------------------
 	// Return on success:
-	//--------------------
-
 	return 0;
 
-	//------------------
 	// Return on error:
-	//------------------
-
 	halt0: return -1;
 }
 
